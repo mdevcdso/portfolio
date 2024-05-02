09f9cb76cdd207cd6186786f43233b48069f2352
mdevcdso <marioncandusso@gmail.com>
2024-05-02 14:28:22 +0200

    mobile folder and update style

--- script.js
+++ script.js
@@ -14,18 +14,27 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 //ADAPTER TAILLE ECRAN
-window.addEventListener("load", function() {
-    var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent);
-    var currentUrl = window.location.pathname;
-    if (isMobile) {
-        if (!currentUrl.includes("/mobile")) {
-            var mobileUrl = "/mobile" + currentUrl;
-            window.location.replace(mobileUrl);
-        }
-    } else {
-        if (currentUrl.includes("/mobile")) {
-            var nonMobileUrl = currentUrl.replace("/mobile", "");
-            window.location.replace(nonMobileUrl);
-        }
-    }
-});
+// window.addEventListener("load", function() {
+//     var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent);
+//     var currentUrl = window.location.pathname;
+//     if (isMobile) {
+//         if (!currentUrl.includes("/mobile")) {
+//             var mobileUrl = "/mobile" + currentUrl;
+//             window.location.replace(mobileUrl);
+//         }
+//     } else {
+//         if (currentUrl.includes("/mobile")) {
+//             var nonMobileUrl = currentUrl.replace("/mobile", "");
+//             window.location.replace(nonMobileUrl);
+//         }
+//     }
+// });
+
+//CHARGER LES FICHIERS MOBILES
+const isMobile = window.matchMedia("(max-width: 768px)");
+
+if (isMobile.matches) {
+  window.location.href = "presentation_mobile.html";
+}
+
+  
